4d11db819c73666bd04f163ee156d9b7588ff19f
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-11-29 17:26:38 +0100

    GPU

--- lib/emulator/src/graphics/mod.rs
+++ lib/emulator/src/graphics/mod.rs
@@ -1,4 +1,67 @@
-
-
-// /graphics 
+// /graphics
 // Tile Sets
+
+// +------+------------+
+// | 0b11 | white      |
+// | 0b10 | dark-gray  |
+// | 0b01 | light-gray |
+// | 0b00 | black      |
+// +------+------------+
+
+const VRAM_BEGIN: usize = 0x8000;
+const VRAM_END: usize = 0x9FFF;
+const VRAM_SIZE: usize = VRAM_END - VRAM_BEGIN + 1;
+
+#[derive(Copy, Clone)]
+enum TilePixelValue {
+    Zero,
+    One,
+    Two,
+    Three,
+}
+
+type Tile = [[TilePixelValue; 8]; 8];
+fn empty_tile() -> Tile {
+    [[TilePixelValue::Zero; 8]; 8]
+}
+
+struct GPU {
+    vram: [u8; VRAM_SIZE],
+    tile_set: [Tile; 384],
+}
+
+impl GPU {
+    fn read_vram(&self, address: usize) -> u8 {
+        self.vram[address]
+    }
+
+    fn write_vram(&mut self, index: usize, value: u8) {
+        self.vram[index] = value;
+        if index >= 0x1800 {
+            return;
+        }
+
+        let normalized_index = index & 0xFFFE;
+
+        let byte1 = self.vram[normalized_index];
+        let byte2 = self.vram[normalized_index + 1];
+
+        let tile_index = index / 16;
+        let row_index = (index % 16) / 2;
+
+        for pixel_index in 0..8 {
+            let mask = 1 << (7 - pixel_index);
+            let lsb = byte1 & mask;
+            let msb = byte2 & mask;
+
+            let value = match (lsb != 0, msb != 0) {
+                (true, true) => TilePixelValue::Three,
+                (false, true) => TilePixelValue::Two,
+                (true, false) => TilePixelValue::One,
+                (false, false) => TilePixelValue::Zero,
+            };
+
+            self.tile_set[tile_index][row_index][pixel_index] = value;
+        }
+    }
+}
